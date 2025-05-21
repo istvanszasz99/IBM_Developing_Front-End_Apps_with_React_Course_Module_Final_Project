b3078ddf9f1dd1cec622753f63ca39fbd13199c0
Szász István Zoltán <istvanszasz99@gmail.com>
2025-05-21 11:33:31 +0200

    Naming Bug???

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/IBM_Developing_Front-End_Apps_with_React_Course_Module_Final_Project",
+  base: "/shoppingreact",
   plugins: [react()],
 })
